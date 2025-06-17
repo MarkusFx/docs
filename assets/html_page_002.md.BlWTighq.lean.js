efee78479756986838a4e7e81a6edd3ea8c917c3
MarkusFx <metroid-corp@yandex.ru>
2025-06-17 22:18:55 +0300

    Updates

---
+++ assets/html_page_002.md.BlWTighq.lean.js
@@ -0,0 +1,123 @@
+import{C as a}from"./chunks/CodePreview.CZAi66Qd.js";import{c as t,o as p,ag as n,G as l,k as s}from"./chunks/framework.BgId8OS5.js";/* empty css                                                                           */const h=`<div>
+    <div>Первый блок</div>
+    <div>Второй блок</div>
+</div>
+`,e="",k="",E=`<p>Абзац</p>
+`,r="",d="",g=`<div>
+    Моя супер <br />
+    длинная строка
+</div>
+`,c="",y="",o=`<pre>
+    Первая строка
+    Вторая строка
+        Третья     строка
+</pre>
+`,m="",u="",b=`<div>
+    Это обычный текст, а
+    <span style="color: red">это красный</span>
+</div>
+`,_="",A="",D=`<div>
+    <h1>Заголовок 1-го уровня</h1>
+    <h2>Заголовок 2-го уровня</h2>
+    <h3>Заголовок 3-го уровня</h3>
+    <h4>Заголовок 4-го уровня</h4>
+    <h5>Заголовок 5-го уровня</h5>
+    <h6>Заголовок 6-го уровня</h6>
+</div>
+`,v="",C="",T=`<div>
+    <b>Жирный текст</b>
+    <strong>Жирный текст (логический)</strong>
+    <i>Курсивный текст</i>
+    <em>Курсивный текст (логический)</em>
+    <s>Зачеркнутый текст</s>
+    <del>Зачеркнутый текст (логический)</del>
+    <u>Подчеркнутый текст</u>
+    <ins>Подчеркнутый текст (логический)</ins>
+    <mark>Выделенный текст</mark>
+    <small>Текст меньший по размеру</small>
+    <sub>Подстрочный текст</sub>
+    <sup>Надстрочный текст</sup>
+</div>
+`,F="",S="",q=`<div>
+    <img src="/images/html/cat-001.png" alt="Кот" height="150px" title="Котэ" />
+</div>
+<div>
+    <img
+        src="/images/html/not_found.png"
+        alt="Это изображение не было найдено"
+    />
+</div>
+`,x="",j="",P=`<ul>
+    <li>Яблоко</li>
+    <li>Банан</li>
+    <li>Вишня</li>
+</ul>
+
+<ol>
+    <li>Первое</li>
+    <li>Второе</li>
+    <li>Третье</li>
+</ol>
+`,I="",V="",B=`<ol type="A">
+    <li>Первый</li>
+    <li>Второй</li>
+</ol>
+`,f="",N="",w=`<dl>
+    <dt>HTML</dt>
+    <dd>Язык разметки для веб-страниц</dd>
+
+    <dt>CSS</dt>
+    <dd>Стиль оформления веб-страниц</dd>
+</dl>
+`,R="",H="",J=`<ul>
+    <li>
+        Фрукты
+        <ul>
+            <li>Яблоко</li>
+            <li>Апельсин</li>
+        </ul>
+    </li>
+    <li>Овощи</li>
+</ul>
+`,L="",M="",O=`<ul style="list-style-type: disc">
+    <li>Элемент 1</li>
+    <li>Элемент 2</li>
+</ul>
+
+<ul style="list-style-type: circle">
+    <li>Элемент 1</li>
+    <li>Элемент 2</li>
+</ul>
+
+<ul style="list-style-type: square">
+    <li>Элемент 1</li>
+    <li>Элемент 2</li>
+</ul>
+
+<ul style="list-style-type: none">
+    <li>Элемент 1</li>
+    <li>Элемент 2</li>
+</ul>
+`,G="",z="",K=`<ol start="5">
+    <li>Пятый</li>
+    <li>Шестой</li>
+</ol>
+`,Q="",U="",W=`<details>
+    <summary>Подробнее</summary>
+    <p>Здесь скрытый текст, который раскроется при клике.</p>
+</details>
+`,X="",Y="",Z=`<details>
+    <summary>Подробнее</summary>
+    <p>Здесь скрытый текст, который раскроется при клике.</p>
+</details>
+
+<details open>
+    <summary>Подробнее</summary>
+    <p>Изначально открыт...</p>
+</details>
+
+<details>
+    <summary>Подробнее</summary>
+    <p>Здесь скрытый текст, который раскроется при клике.</p>
+</details>
+`,$="",ss="",hs=JSON.parse('{"title":"Элементы","description":"","frontmatter":{},"headers":[],"relativePath":"html/page_002.md","filePath":"html/page_002.md"}'),is={name:"html/page_002.md"},es=Object.assign(is,{setup(as){return(ns,i)=>(p(),t("div",null,[i[0]||(i[0]=n("",6)),l(a,{html:s(h),css:s(e),js:s(k),height:"100px"},null,8,["html","css","js"]),i[1]||(i[1]=n("",4)),l(a,{html:s(E),css:s(r),js:s(d),height:"100px"},null,8,["html","css","js"]),i[2]||(i[2]=n("",5)),l(a,{html:s(g),css:s(c),js:s(y),height:"100px"},null,8,["html","css","js"]),i[3]||(i[3]=n("",4)),l(a,{html:s(o),css:s(m),js:s(u),height:"100px"},null,8,["html","css","js"]),i[4]||(i[4]=n("",4)),l(a,{html:s(b),css:s(_),js:s(A),height:"100px"},null,8,["html","css","js"]),i[5]||(i[5]=n("",4)),l(a,{html:s(D),css:s(v),js:s(C),height:"350px"},null,8,["html","css","js"]),i[6]||(i[6]=n("",4)),l(a,{html:s(T),css:s(F),js:s(S),height:"150px"},null,8,["html","css","js"]),i[7]||(i[7]=n("",6)),l(a,{html:s(q),css:s(x),js:s(j),height:"250px"},null,8,["html","css","js"]),i[8]||(i[8]=n("",5)),l(a,{html:s(P),css:s(I),js:s(V),height:"200px"},null,8,["html","css","js"]),i[9]||(i[9]=n("",3)),l(a,{html:s(J),css:s(L),js:s(M),height:"150px"},null,8,["html","css","js"]),i[10]||(i[10]=n("",3)),l(a,{html:s(O),css:s(G),js:s(z),height:"250px"},null,8,["html","css","js"]),i[11]||(i[11]=n("",4)),l(a,{html:s(B),css:s(f),js:s(N),height:"100px"},null,8,["html","css","js"]),i[12]||(i[12]=n("",3)),l(a,{html:s(K),css:s(Q),js:s(U),height:"100px"},null,8,["html","css","js"]),i[13]||(i[13]=n("",4)),l(a,{html:s(w),css:s(R),js:s(H),height:"150px"},null,8,["html","css","js"]),i[14]||(i[14]=n("",4)),l(a,{html:s(W),css:s(X),js:s(Y),height:"150px"},null,8,["html","css","js"]),i[15]||(i[15]=n("",3)),l(a,{html:s(Z),css:s($),js:s(ss),height:"200px"},null,8,["html","css","js"])]))}});export{hs as __pageData,es as default};
