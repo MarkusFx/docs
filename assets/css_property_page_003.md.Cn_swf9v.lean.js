838e9f39160e69546f4d4457a006864c087e54b2
MarkusFx <metroid-corp@yandex.ru>
2025-07-27 18:56:26 +0300

    Updates

---
+++ assets/css_property_page_003.md.Cn_swf9v.lean.js
@@ -0,0 +1,50 @@
+import{C as a}from"./chunks/theme.76jzxK6f.js";import{c as t,o as p,ag as n,G as l,k as s}from"./chunks/framework.Drb80Ych.js";const e=`<ul class="custom-list-ul">
+    <li>Пункт 1</li>
+    <li>Пункт 2</li>
+    <li>Пункт 3</li>
+</ul>
+
+<ol class="custom-list-ol">
+    <li>Пункт 1</li>
+    <li>Пункт 2</li>
+    <li>Пункт 3</li>
+</ol>
+`,h=`.custom-list-ul {
+    list-style-type: square;
+}
+
+.custom-list-ol {
+    list-style-type: decimal-leading-zero;
+}
+`,k="",E=`<ol class="pepe-img">
+    <li>Пункт 1</li>
+    <li>Пункт 2</li>
+    <li>Пункт 3</li>
+    <li>Пункт 4</li>
+</ol>
+`,r=`.pepe-img {
+    list-style-image: url('/images/css/pepe-002-icon.png');
+}
+`,d="",g=`<ul>
+    <li>Пункт 1</li>
+    <li>Пункт 2</li>
+    <li>Пункт 3</li>
+</ul>
+
+<ul class="custom-list">
+    <li>Пункт 1</li>
+    <li>Пункт 2</li>
+    <li>Пункт 3</li>
+</ul>
+`,c=`.custom-list {
+    list-style-position: inside;
+}
+`,o="",y=`<ul class="custom-list">
+    <li>Пункт 1</li>
+    <li>Пункт 2</li>
+    <li>Пункт 3</li>
+</ul>
+`,u=`.custom-list {
+    list-style: square inside none;
+}
+`,m="",v=JSON.parse('{"title":"Списки","description":"","frontmatter":{"outline":[2,2]},"headers":[],"relativePath":"css/property/page_003.md","filePath":"css/property/page_003.md"}'),b={name:"css/property/page_003.md"},D=Object.assign(b,{setup(_){return(C,i)=>(p(),t("div",null,[i[0]||(i[0]=n("",9)),l(a,{html:s(e),css:s(h),js:s(k),height:"200px"},null,8,["html","css","js"]),i[1]||(i[1]=n("",6)),l(a,{html:s(E),css:s(r),js:s(d),height:"300px"},null,8,["html","css","js"]),i[2]||(i[2]=n("",6)),l(a,{html:s(g),css:s(c),js:s(o),height:"200px"},null,8,["html","css","js"]),i[3]||(i[3]=n("",6)),l(a,{html:s(y),css:s(u),js:s(m),height:"160px"},null,8,["html","css","js"])]))}});export{v as __pageData,D as default};
