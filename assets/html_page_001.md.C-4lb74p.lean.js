2554f08bb20a38a089a04283fe0369935b201a36
MarkusFx <metroid-corp@yandex.ru>
2025-08-01 21:11:26 +0300

    Updates

--- assets/html_page_001.md.C-4lb74p.lean.js
+++
@@ -1,4 +0,0 @@
-import{C as t}from"./chunks/theme.D8lkRoLb.js";import{c as l,o as n,ag as a,G as e,k as i}from"./chunks/framework.Drb80Ych.js";const h=`<h1>Привет, мир!</h1>
-<p>Это мой первый HTML-документ.</p>
-<a href="https://markusfx.ru" target="_blank">Перейти по ссылке</a>
-`,p="",k="",c=JSON.parse('{"title":"Введение","description":"","frontmatter":{"outline":[2]},"headers":[],"relativePath":"html/page_001.md","filePath":"html/page_001.md"}'),d={name:"html/page_001.md"},u=Object.assign(d,{setup(r){return(E,s)=>(n(),l("div",null,[s[0]||(s[0]=a("",8)),e(t,{html:i(h),css:i(p),js:i(k),height:"300px"},null,8,["html","css","js"]),s[1]||(s[1]=a("",13))]))}});export{c as __pageData,u as default};
