a15ae4e11bf8ea597304b415398b1cdc562eaf00
MarkusFx <metroid-corp@yandex.ru>
2025-08-04 10:23:55 +0300

    Updates

---
+++ assets/html_forms_page_001.md.BEqoS7yz.lean.js
@@ -0,0 +1,14 @@
+import{C as a}from"./chunks/theme.BsvUnSYj.js";import{c as n,o as t,ag as l,G as e,k as s}from"./chunks/framework.Drb80Ych.js";const p=`<form action="/submit" method="post">
+    <label for="name">Имя:</label>
+    <input type="text" id="name" name="name" required />
+
+    <br />
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required />
+
+    <br />
+
+    <button type="submit">Отправить</button>
+</form>
+`,h="",k="",c=JSON.parse('{"title":"Создание формы","description":"","frontmatter":{"outline":[2,3]},"headers":[],"relativePath":"html/forms/page_001.md","filePath":"html/forms/page_001.md"}'),r={name:"html/forms/page_001.md"},y=Object.assign(r,{setup(E){return(o,i)=>(t(),n("div",null,[i[0]||(i[0]=l("",5)),e(a,{html:s(p),css:s(h),js:s(k),height:"150px"},null,8,["html","css","js"])]))}});export{c as __pageData,y as default};
