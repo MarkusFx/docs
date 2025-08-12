0e7a68ec31154c3c9f0eeb47a031545bae4a0073
MarkusFx <metroid-corp@yandex.ru>
2025-08-12 17:09:45 +0300

    Updates

---
+++ assets/html_elements_page_004.md.CyiuORfI.lean.js
@@ -0,0 +1,10 @@
+import{C as a}from"./chunks/theme.QvOKsbcg.js";import{c as n,o as t,ag as l,G as e,k as s}from"./chunks/framework.Drb80Ych.js";const p=`<div>
+    <img src="/images/html/cat-001.png" alt="Кот" height="150px" title="Котэ" />
+</div>
+<div>
+    <img
+        src="/images/html/not_found.png"
+        alt="Это изображение не было найдено"
+    />
+</div>
+`,h="",k="",c=JSON.parse('{"title":"Изображения","description":"","frontmatter":{"outline":[2,3]},"headers":[],"relativePath":"html/elements/page_004.md","filePath":"html/elements/page_004.md"}'),r={name:"html/elements/page_004.md"},m=Object.assign(r,{setup(d){return(E,i)=>(t(),n("div",null,[i[0]||(i[0]=l("",6)),e(a,{html:s(p),css:s(h),js:s(k),height:"250px"},null,8,["html","css","js"])]))}});export{c as __pageData,m as default};
