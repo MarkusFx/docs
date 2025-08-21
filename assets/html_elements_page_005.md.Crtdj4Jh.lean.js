d346a211a1db866fbd38f6e29405f1806fee75c4
MarkusFx <metroid-corp@yandex.ru>
2025-08-21 14:46:04 +0300

    Updates

---
+++ assets/html_elements_page_005.md.Crtdj4Jh.lean.js
@@ -0,0 +1,56 @@
+import{C as a}from"./chunks/theme.Bh7r4QRp.js";import{c as t,o as p,ag as l,G as n,k as s}from"./chunks/framework.Drb80Ych.js";const h=`<ul>
+    <li>Яблоко</li>
+    <li>Банан</li>
+    <li>Вишня</li>
+</ul>
+
+<ol>
+    <li>Первое</li>
+    <li>Второе</li>
+    <li>Третье</li>
+</ol>
+`,e="",k="",E=`<ol type="A">
+    <li>Первый</li>
+    <li>Второй</li>
+</ol>
+`,r="",d="",g=`<dl>
+    <dt>HTML</dt>
+    <dd>Язык разметки для веб-страниц</dd>
+
+    <dt>CSS</dt>
+    <dd>Стиль оформления веб-страниц</dd>
+</dl>
+`,c="",y="",o=`<ul>
+    <li>
+        Фрукты
+        <ul>
+            <li>Яблоко</li>
+            <li>Апельсин</li>
+        </ul>
+    </li>
+    <li>Овощи</li>
+</ul>
+`,b="",u="",m=`<ul style="list-style-type: disc">
+    <li>Элемент 1</li>
+    <li>Элемент 2</li>
+</ul>
+
+<ul style="list-style-type: circle">
+    <li>Элемент 1</li>
+    <li>Элемент 2</li>
+</ul>
+
+<ul style="list-style-type: square">
+    <li>Элемент 1</li>
+    <li>Элемент 2</li>
+</ul>
+
+<ul style="list-style-type: none">
+    <li>Элемент 1</li>
+    <li>Элемент 2</li>
+</ul>
+`,A="",_="",v=`<ol start="5">
+    <li>Пятый</li>
+    <li>Шестой</li>
+</ol>
+`,D="",C="",j=JSON.parse('{"title":"Списки","description":"","frontmatter":{"outline":[2,3]},"headers":[],"relativePath":"html/elements/page_005.md","filePath":"html/elements/page_005.md"}'),T={name:"html/elements/page_005.md"},I=Object.assign(T,{setup(x){return(F,i)=>(p(),t("div",null,[i[0]||(i[0]=l("",5)),n(a,{html:s(h),css:s(e),js:s(k),height:"200px"},null,8,["html","css","js"]),i[1]||(i[1]=l("",2)),n(a,{html:s(o),css:s(b),js:s(u),height:"150px"},null,8,["html","css","js"]),i[2]||(i[2]=l("",2)),n(a,{html:s(m),css:s(A),js:s(_),height:"250px"},null,8,["html","css","js"]),i[3]||(i[3]=l("",5)),n(a,{html:s(E),css:s(r),js:s(d),height:"100px"},null,8,["html","css","js"]),i[4]||(i[4]=l("",2)),n(a,{html:s(v),css:s(D),js:s(C),height:"100px"},null,8,["html","css","js"]),i[5]||(i[5]=l("",4)),n(a,{html:s(g),css:s(c),js:s(y),height:"150px"},null,8,["html","css","js"])]))}});export{j as __pageData,I as default};
