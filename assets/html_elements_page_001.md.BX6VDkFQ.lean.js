ec69271abf07548106bf03468942b93293c519fb
MarkusFx <metroid-corp@yandex.ru>
2025-08-25 20:35:28 +0300

    Updates

---
+++ assets/html_elements_page_001.md.BX6VDkFQ.lean.js
@@ -0,0 +1,19 @@
+import{C as a}from"./chunks/theme.ChGxLw5c.js";import{c as t,o as e,ag as n,G as l,k as s}from"./chunks/framework.Drb80Ych.js";const p=`<div>
+    <div>Первый блок</div>
+    <div>Второй блок</div>
+</div>
+`,h="",k="",d=`<p>Абзац</p>
+`,r="",E="",c=`<div>
+    Моя супер <br />
+    длинная строка
+</div>
+`,g="",o="",b=`<pre>
+    Первая строка
+    Вторая строка
+        Третья     строка
+</pre>
+`,m="",u="",y=`<div>
+    Это обычный текст, а
+    <span style="color: red">это красный</span>
+</div>
+`,v="",_="",P=JSON.parse('{"title":"Элементы группировки","description":"","frontmatter":{"outline":[2,3]},"headers":[],"relativePath":"html/elements/page_001.md","filePath":"html/elements/page_001.md"}'),A={name:"html/elements/page_001.md"},q=Object.assign(A,{setup(x){return(D,i)=>(e(),t("div",null,[i[0]||(i[0]=n("",5)),l(a,{html:s(p),css:s(h),js:s(k),height:"100px"},null,8,["html","css","js"]),i[1]||(i[1]=n("",4)),l(a,{html:s(d),css:s(r),js:s(E),height:"100px"},null,8,["html","css","js"]),i[2]||(i[2]=n("",5)),l(a,{html:s(c),css:s(g),js:s(o),height:"100px"},null,8,["html","css","js"]),i[3]||(i[3]=n("",4)),l(a,{html:s(b),css:s(m),js:s(u),height:"100px"},null,8,["html","css","js"]),i[4]||(i[4]=n("",4)),l(a,{html:s(y),css:s(v),js:s(_),height:"100px"},null,8,["html","css","js"])]))}});export{P as __pageData,q as default};
