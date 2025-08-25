0105094eb4030173c6a30bab5a66bee12b725516
MarkusFx <metroid-corp@yandex.ru>
2025-08-25 09:48:57 +0300

    Updates

---
+++ assets/html_forms_page_002.md.Dv5nltY5.lean.js
@@ -0,0 +1,67 @@
+import{C as a}from"./chunks/theme.CFVi88bv.js";import{c as l,o as h,ag as t,G as n,k as s}from"./chunks/framework.Drb80Ych.js";const p=`<p>text: <input type="text" value="test" /></p>
+<p>email: <input type="email" value="mail@mail.ru" /></p>
+<p>password: <input type="password" value="test" /></p>
+<p>file: <input type="file" /></p>
+<p>submit: <input type="submit" /></p>
+<p>number: <input type="number" value="300.33" /></p>
+<p>date: <input type="date" /></p>
+<p>checkbox_1: <input type="checkbox" checked /></p>
+<p>checkbox_2: <input type="checkbox" /></p>
+<p>radio_1: <input type="radio" name="myRadio" checked /></p>
+<p>radio_2: <input type="radio" name="myRadio" /></p>
+`,k="",e="",E=`<textarea name="message" rows="6" cols="50">Текст...</textarea>
+`,r="",d="",g=`<p>
+    <select>
+        <option value="cat">Кот</option>
+        <option value="dog" selected>Собака</option>
+        <option value="fish" disabled>Рыба</option>
+    </select>
+</p>
+
+<p>
+    <label for="totem">Выбери тотемное животное (удерживаем Ctrl/Cmd): </label
+    ><br />
+    <select id="totem" multiple>
+        <option value="cat">Кот</option>
+        <option value="dog">Собака</option>
+        <option value="fish">Рыба</option>
+    </select>
+</p>
+
+<p>
+    <select>
+        <optgroup label="Смартфоны">
+            <option value="iphone">iPhone</option>
+            <option value="samsung">Samsung</option>
+            <option value="LG">LG</option>
+        </optgroup>
+        <optgroup label="Игровые приставки">
+            <option value="ps">PS</option>
+            <option value="xbox">XBOX</option>
+        </optgroup>
+    </select>
+</p>
+`,o="",y="",c=`<label for="name">Имя:</label>
+<input type="text" id="name" name="name" />
+`,u="",F="",b=`<button type="submit">Отправить</button>
+<button type="reset">Сбросить</button>
+<button type="button">Кнопка без действия</button>
+`,m="",C="",v=`<fieldset>
+    <legend>Личные данные</legend>
+    <p>Имя: <input type="text" name="firstname" /></p>
+    <p>Фамилия: <input type="text" name="lastname" /></p>
+</fieldset>
+`,q="",A="",B=`<input list="browsers" name="browser" />
+<datalist id="browsers">
+    <option value="Chrome"></option>
+    <option value="Firefox"></option>
+    <option value="Edge"></option>
+</datalist>
+`,D="",_="",x=`<form oninput="result.value = Number(a.value) + Number(b.value)">
+    <input type="number" id="a" value="49" />
+    +
+    <input type="number" id="b" value="51" />
+    =
+    <output name="result" for="a b">100</output>
+</form>
+`,f="",T="",I=JSON.parse('{"title":"Элементы форм","description":"","frontmatter":{"outline":[2,3]},"headers":[],"relativePath":"html/forms/page_002.md","filePath":"html/forms/page_002.md"}'),P={name:"html/forms/page_002.md"},N=Object.assign(P,{setup(S){return(j,i)=>(h(),l("div",null,[i[0]||(i[0]=t("",5)),n(a,{html:s(p),css:s(k),js:s(e),height:"480px"},null,8,["html","css","js"]),i[1]||(i[1]=t("",4)),n(a,{html:s(E),css:s(r),js:s(d),height:"150px"},null,8,["html","css","js"]),i[2]||(i[2]=t("",4)),n(a,{html:s(g),css:s(o),js:s(y),height:"380px"},null,8,["html","css","js"]),i[3]||(i[3]=t("",5)),n(a,{html:s(c),css:s(u),js:s(F),height:"100px"},null,8,["html","css","js"]),i[4]||(i[4]=t("",4)),n(a,{html:s(b),css:s(m),js:s(C),height:"100px"},null,8,["html","css","js"]),i[5]||(i[5]=t("",4)),n(a,{html:s(v),css:s(q),js:s(A),height:"200px"},null,8,["html","css","js"]),i[6]||(i[6]=t("",4)),n(a,{html:s(B),css:s(D),js:s(_),height:"150px"},null,8,["html","css","js"]),i[7]||(i[7]=t("",4)),n(a,{html:s(x),css:s(f),js:s(T),height:"150px"},null,8,["html","css","js"])]))}});export{I as __pageData,N as default};
