b51c6d73452de7644de678f4c654e4d06dff6135
MarkusFx <metroid-corp@yandex.ru>
2025-08-26 14:08:43 +0300

    Updates

---
+++ assets/css_property_page_008.md.DEFVGoh0.lean.js
@@ -0,0 +1,16 @@
+import{C as a}from"./chunks/theme.BE6tHNFg.js";import{c as n,o as l,ag as t,G as e,k as s}from"./chunks/framework.Drb80Ych.js";const p=`<div class="outlined-box">Блок с контуром</div>
+<button class="button">Кнопка с контуром при наведении</button>
+`,h=`.outlined-box {
+    margin: 20px;
+    width: 200px;
+    height: 100px;
+    background: #00e5a4;
+    outline: 3px dashed #5c73e7;
+    outline-offset: 5px;
+}
+
+.button:hover {
+    outline: 3px solid #3e63dd;
+    outline-offset: 2px;
+}
+`,k="",c=JSON.parse('{"title":"Контур","description":"","frontmatter":{"outline":[2,2]},"headers":[],"relativePath":"css/property/page_008.md","filePath":"css/property/page_008.md"}'),r={name:"css/property/page_008.md"},u=Object.assign(r,{setup(d){return(o,i)=>(l(),n("div",null,[i[0]||(i[0]=t("",6)),e(a,{html:s(p),css:s(h),js:s(k),height:"250px"},null,8,["html","css","js"])]))}});export{c as __pageData,u as default};
