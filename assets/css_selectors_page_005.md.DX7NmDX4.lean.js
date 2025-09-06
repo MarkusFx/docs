8f5a785f60a59bdbcccb3016a6c2490543daedc3
MarkusFx <metroid-corp@yandex.ru>
2025-09-06 18:18:16 +0300

    Updates

---
+++ assets/css_selectors_page_005.md.DX7NmDX4.lean.js
@@ -0,0 +1,25 @@
+import{C as a}from"./chunks/theme.D9CtuBiQ.js";import{c as t,o as l,ag as n,G as e,k as s}from"./chunks/framework.Drb80Ych.js";const p=`<p class="note">Это важная заметка.</p>
+`,h=`.note::before {
+    content: 'Внимание! ';
+    color: red;
+}
+`,r="",d=`<p class="note">Это важная заметка.</p>
+`,k=`.note::after {
+    content: ' Спасибо за внимание!';
+    color: green;
+}
+`,c="",o=`<p>Заголовок параграфа начинается с большой буквы.</p>
+`,E=`p::first-letter {
+    font-size: 2em;
+    color: red;
+    font-weight: bold;
+}
+`,g="",b=`<p>
+    Это текст с несколькими строками. При уменьшении ширины окна можно увидеть,
+    как стилизуется только первая строка текста, а остальные - нет.
+</p>
+`,u=`p::first-line {
+    background: #a8b1ff;
+    text-transform: uppercase;
+}
+`,m="",f=JSON.parse('{"title":"Псевдоэлементы","description":"","frontmatter":{"outline":[2,3]},"headers":[],"relativePath":"css/selectors/page_005.md","filePath":"css/selectors/page_005.md"}'),v={name:"css/selectors/page_005.md"},x=Object.assign(v,{setup(y){return(C,i)=>(l(),t("div",null,[i[0]||(i[0]=n("",7)),e(a,{html:s(p),css:s(h),js:s(r),height:"100px"},null,8,["html","css","js"]),i[1]||(i[1]=n("",6)),e(a,{html:s(d),css:s(k),js:s(c),height:"100px"},null,8,["html","css","js"]),i[2]||(i[2]=n("",6)),e(a,{html:s(o),css:s(E),js:s(g),height:"100px"},null,8,["html","css","js"]),i[3]||(i[3]=n("",6)),e(a,{html:s(b),css:s(u),js:s(m),height:"150px"},null,8,["html","css","js"])]))}});export{f as __pageData,x as default};
