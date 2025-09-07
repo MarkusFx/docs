96a980a4d708e2ac03e3ac0779554ff0d732dd57
MarkusFx <metroid-corp@yandex.ru>
2025-09-07 12:43:53 +0300

    Updates

--- assets/css_selectors_page_006.md.sgNTY2JX.lean.js
+++
@@ -1,9 +0,0 @@
-import{C as a}from"./chunks/theme.Cy7xNdmk.js";import{c as n,o as l,ag as t,G as e,k as s}from"./chunks/framework.Drb80Ych.js";const p=`<p style="color: red">Некоторый текст</p>
-`,r=`p {
-    color: green;
-}
-`,h="",d=`<p style="color: red">Некоторый текст</p>
-`,o=`p {
-    color: green !important;
-}
-`,c="",u=JSON.parse('{"title":"!important","description":"","frontmatter":{"outline":[2,3]},"headers":[],"relativePath":"css/selectors/page_006.md","filePath":"css/selectors/page_006.md"}'),k={name:"css/selectors/page_006.md"},v=Object.assign(k,{setup(g){return(E,i)=>(l(),n("div",null,[i[0]||(i[0]=t("",7)),e(a,{html:s(p),css:s(r),js:s(h),height:"100px"},null,8,["html","css","js"]),i[1]||(i[1]=t("",5)),e(a,{html:s(d),css:s(o),js:s(c),height:"100px"},null,8,["html","css","js"])]))}});export{u as __pageData,v as default};
