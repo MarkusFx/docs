84763a5d1fb0a99ce10ac7798770c1acb5979d85
MarkusFx <metroid-corp@yandex.ru>
2025-09-15 00:36:14 +0300

    Updates

---
+++ assets/css_property_page_005.md.DskgjVVm.lean.js
@@ -0,0 +1,18 @@
+import{C as n}from"./chunks/theme.DdP-nw4A.js";import{c as t,o as l,ag as a,G as e,k as i}from"./chunks/framework.Drb80Ych.js";const p=`<div class="box-padding">Я внутри с отступом!</div>
+`,r=`.box-padding {
+    background: lightblue;
+    padding: 20px;
+    border: 2px solid blue;
+}
+.box-margin {
+    background: lightgreen;
+    margin: 20px;
+    border: 2px solid green;
+}
+`,h="",d=`<div class="box-margin">Я с внешним отступом!</div>
+`,k=`.box-margin {
+    background: lightgreen;
+    margin: 20px;
+    border: 2px solid green;
+}
+`,o="",u=JSON.parse('{"title":"Отступы","description":"","frontmatter":{"outline":[2,2]},"headers":[],"relativePath":"css/property/page_005.md","filePath":"css/property/page_005.md"}'),g={name:"css/property/page_005.md"},y=Object.assign(g,{setup(c){return(E,s)=>(l(),t("div",null,[s[0]||(s[0]=a("",7)),e(n,{html:i(p),css:i(r),js:i(h),height:"180px"},null,8,["html","css","js"]),s[1]||(s[1]=a("",8)),e(n,{html:i(d),css:i(k),js:i(o),height:"180px"},null,8,["html","css","js"]),s[2]||(s[2]=a("",2))]))}});export{u as __pageData,y as default};
