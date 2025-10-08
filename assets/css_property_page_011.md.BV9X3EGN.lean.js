2c273a90acf661ddeb736ecc5ddf10e57526fcef
MarkusFx <metroid-corp@yandex.ru>
2025-10-08 23:00:27 +0300

    Updates

---
+++ assets/css_property_page_011.md.BV9X3EGN.lean.js
@@ -0,0 +1,18 @@
+import{C as a}from"./chunks/theme.Cf19Ejia.js";import{c as n,o as p,ag as l,G as t,k as s}from"./chunks/framework.Drb80Ych.js";const h=`<div class="box">Блок с тенью</div>
+<div class="text">Текст с тенью</div>
+
+<div class="animated-shadow"></div>
+`,k=`.box {
+    width: 200px;
+    height: 100px;
+    background: #fff;
+    box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.4);
+    margin-bottom: 20px;
+}
+
+.text {
+    font-size: 32px;
+    text-shadow: 3px 3px 5px #000;
+    padding: 10px;
+}
+`,e="",c=JSON.parse('{"title":"Тень","description":"","frontmatter":{"outline":[2,2]},"headers":[],"relativePath":"css/property/page_011.md","filePath":"css/property/page_011.md"}'),r={name:"css/property/page_011.md"},y=Object.assign(r,{setup(d){return(E,i)=>(p(),n("div",null,[i[0]||(i[0]=l("",6)),t(a,{html:s(h),css:s(k),js:s(e),height:"250px"},null,8,["html","css","js"])]))}});export{c as __pageData,y as default};
