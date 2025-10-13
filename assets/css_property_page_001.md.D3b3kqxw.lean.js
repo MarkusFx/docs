d29f498f13de1616a0b1a280cb38cc2347a2d356
MarkusFx <metroid-corp@yandex.ru>
2025-10-13 18:17:32 +0300

    Updates

---
+++ assets/css_property_page_001.md.D3b3kqxw.lean.js
@@ -0,0 +1,54 @@
+import{C as a}from"./chunks/theme.DN2A8tas.js";import{c as p,o as t,ag as n,G as l,k as s}from"./chunks/framework.Drb80Ych.js";const e=`<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Пример</title>
+        <link rel="stylesheet" href="styles.css" />
+    </head>
+    <body>
+        <p class="example_1">Параграф 1</p>
+        <p class="example_2">Параграф 2</p>
+        <p class="example_3">Параграф 3</p>
+        <p class="example_4">Параграф 4</p>
+    </body>
+</html>
+`,h=`.example_1 {
+    color: red;
+}
+
+.example_2 {
+    color: #ff0000;
+}
+
+.example_3 {
+    color: rgb(255, 0, 0);
+}
+
+.example_4 {
+    color: hsl(0, 100%, 50%);
+}
+`,k="",r=`<div class="box">Текст с фоном</div>
+`,E=`.box {
+    background-color: lightblue;
+}
+`,d="",o=`<p class="highlight">Текст с рамкой</p>
+`,c=`.highlight {
+    border: 2px solid; /* задаём толщину и стиль рамки */
+    border-color: orange; /* задаём цвет рамки */
+}
+`,g="",y=`<div class="box_1">Блок</div>
+<div class="box_2">Полупрозрачный блок</div>
+`,b=`.box_1,
+.box_2 {
+    background: blue;
+    color: white;
+}
+
+.box_2 {
+    opacity: 1;
+}
+
+.box_2 {
+    opacity: 0.5;
+}
+`,u="",B=JSON.parse('{"title":"Цвет","description":"","frontmatter":{"outline":[2,2]},"headers":[],"relativePath":"css/property/page_001.md","filePath":"css/property/page_001.md"}'),m={name:"css/property/page_001.md"},x=Object.assign(m,{setup(F){return(C,i)=>(t(),p("div",null,[i[0]||(i[0]=n("",9)),l(a,{html:s(e),css:s(h),js:s(k),height:"200px"},null,8,["html","css","js"]),i[1]||(i[1]=n("",6)),l(a,{html:s(r),css:s(E),js:s(d),height:"100px"},null,8,["html","css","js"]),i[2]||(i[2]=n("",6)),l(a,{html:s(o),css:s(c),js:s(g),height:"100px"},null,8,["html","css","js"]),i[3]||(i[3]=n("",6)),l(a,{html:s(y),css:s(b),js:s(u),height:"100px"},null,8,["html","css","js"])]))}});export{B as __pageData,x as default};
