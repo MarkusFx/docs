9a81a79769e2413d5961675ab7ccf40f794097ce
MarkusFx <metroid-corp@yandex.ru>
2025-10-17 00:54:27 +0300

    Updates

---
+++ assets/css_layout_page_003.md.BqPF1GFU.lean.js
@@ -0,0 +1,64 @@
+import{C as a}from"./chunks/theme.DoemcSeu.js";import{c as n,o as l,ag as p,G as h,k as s}from"./chunks/framework.Drb80Ych.js";const t=`<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8" />
+        <title>flex</title>
+    </head>
+    <body>
+        <header>
+            <h1>Заголовок сайта</h1>
+        </header>
+
+        <nav>Меню: Главная | О нас | Контакты</nav>
+
+        <div class="sidebar">
+            <h3>Боковая панель</h3>
+            <p>Здесь может быть меню, ссылки, виджеты.</p>
+        </div>
+
+        <div class="content">
+            <h2>Основной контент</h2>
+            <p>
+                Это основной текст сайта, который занимает большую часть ширины.
+            </p>
+        </div>
+
+        <footer>© 2025 Мой сайт</footer>
+    </body>
+</html>
+`,k=`body {
+    margin: 0;
+    font-family: sans-serif;
+}
+header {
+    background: #333;
+    color: #fff;
+    padding: 20px;
+    text-align: center;
+}
+nav {
+    background: #555;
+    color: #fff;
+    padding: 10px;
+}
+.sidebar {
+    float: left;
+    width: 30%;
+    background: #f4f4f4;
+    padding: 15px;
+    box-sizing: border-box;
+}
+.content {
+    float: left;
+    width: 70%;
+    padding: 15px;
+    box-sizing: border-box;
+}
+footer {
+    clear: both;
+    background: #333;
+    color: #fff;
+    padding: 10px;
+    text-align: center;
+}
+`,e="",y=JSON.parse('{"title":"Верстка на float","description":"","frontmatter":{"outline":[2,3]},"headers":[],"relativePath":"css/layout/page_003.md","filePath":"css/layout/page_003.md"}'),E={name:"css/layout/page_003.md"},b=Object.assign(E,{setup(r){return(d,i)=>(l(),n("div",null,[i[0]||(i[0]=p("",5)),h(a,{html:s(t),css:s(k),js:s(e),height:"500px"},null,8,["html","css","js"])]))}});export{y as __pageData,b as default};
