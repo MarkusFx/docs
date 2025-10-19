22caeea36f7b077f802943892258395aed73c50e
MarkusFx <metroid-corp@yandex.ru>
2025-10-20 02:59:58 +0300

    Updates

---
+++ assets/css_layout_page_002.md.DCreDUmm.lean.js
@@ -0,0 +1,53 @@
+import{C as a}from"./chunks/theme.CtLi_ihh.js";import{c as n,o as l,ag as p,G as h,k as s}from"./chunks/framework.Drb80Ych.js";const t=`<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Блочная верстка</title>
+    </head>
+    <body>
+        <header>
+            <h1>Заголовок сайта</h1>
+        </header>
+
+        <nav>Меню: Главная | О нас | Контакты</nav>
+
+        <div class="content">
+            <h2>Основной контент</h2>
+            <p>Это основной текст сайта.</p>
+        </div>
+
+        <footer>© 2025 Мой сайт</footer>
+    </body>
+</html>
+`,k=`body {
+    margin: 0;
+    font-family: sans-serif;
+}
+
+header {
+    background: #333;
+    color: #fff;
+    padding: 20px;
+    text-align: center;
+}
+
+nav {
+    background: #555;
+    color: #fff;
+    padding: 10px;
+}
+
+.content {
+    background: #eaeaea;
+    padding: 15px;
+    margin-top: 10px;
+}
+
+footer {
+    background: #333;
+    color: #fff;
+    padding: 10px;
+    text-align: center;
+    margin-top: 10px;
+}
+`,e="",y=JSON.parse('{"title":"Блочная верстка","description":"","frontmatter":{"outline":[2,3]},"headers":[],"relativePath":"css/layout/page_002.md","filePath":"css/layout/page_002.md"}'),E={name:"css/layout/page_002.md"},b=Object.assign(E,{setup(r){return(d,i)=>(l(),n("div",null,[i[0]||(i[0]=p("",5)),h(a,{html:s(t),css:s(k),js:s(e),height:"500px"},null,8,["html","css","js"])]))}});export{y as __pageData,b as default};
