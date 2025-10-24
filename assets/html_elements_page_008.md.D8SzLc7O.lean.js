ff22e2468d0d2be52d5ea98ff27e72cea9bc02a4
MarkusFx <metroid-corp@yandex.ru>
2025-10-24 19:18:29 +0300

    Updates

---
+++ assets/html_elements_page_008.md.D8SzLc7O.lean.js
@@ -0,0 +1,5 @@
+import{C as a}from"./chunks/theme.CCH3Ebd3.js";import{c as e,o as n,ag as t,G as l,k as s}from"./chunks/framework.Drb80Ych.js";const h=`<a href="https://markusfx.ru" target="_blank">Перейти на сайт</a>
+`,p="",k="",d=`<a href="https://markusfx.ru" target="_blank">
+    <img src="/images/html/cat-001.png" alt="Кот-ссылка" height="150px" />
+</a>
+`,r="",o="",b=JSON.parse('{"title":"Ссылки","description":"","frontmatter":{"outline":[2,3]},"headers":[],"relativePath":"html/elements/page_008.md","filePath":"html/elements/page_008.md"}'),E={name:"html/elements/page_008.md"},v=Object.assign(E,{setup(c){return(g,i)=>(n(),e("div",null,[i[0]||(i[0]=t("",4)),l(a,{html:s(h),css:s(p),js:s(k),height:"100px"},null,8,["html","css","js"]),i[1]||(i[1]=t("",13)),l(a,{html:s(d),css:s(r),js:s(o),height:"200px"},null,8,["html","css","js"])]))}});export{b as __pageData,v as default};
