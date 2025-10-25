ec24896e262eb71c6c465e308cb3963cb2cddd32
MarkusFx <metroid-corp@yandex.ru>
2025-10-25 09:07:38 +0300

    Updates

---
+++ assets/css_layout_page_004.md.CyqqFeFU.lean.js
@@ -0,0 +1,72 @@
+import{C as a}from"./chunks/theme.BFRPwaF8.js";import{c as n,o as l,ag as p,G as t,k as s}from"./chunks/framework.Drb80Ych.js";const h=`<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Табличная верстка</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td colspan="2" class="header">Заголовок сайта</td>
+            </tr>
+            <tr>
+                <td class="sidebar">
+                    <p>Боковая панель</p>
+                    <p>Меню, ссылки</p>
+                </td>
+                <td class="content">
+                    <p>Основной контент</p>
+                    <p>Здесь находится основной текст страницы.</p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" class="footer">© 2025 Мой сайт</td>
+            </tr>
+        </table>
+    </body>
+</html>
+`,k=`* {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    font-family: sans-serif;
+}
+
+table {
+    border-collapse: collapse;
+    width: 100%;
+    margin: 0 auto;
+}
+
+td {
+    border: 1px solid #333;
+    padding: 10px;
+}
+
+.header {
+    background: #333;
+    color: #fff;
+    text-align: center;
+    font-size: 20px;
+}
+
+.sidebar {
+    background: #f4f4f4;
+    width: 200px;
+    vertical-align: top;
+}
+
+.content {
+    background: #eaeaea;
+    vertical-align: top;
+    height: 300px;
+}
+
+.footer {
+    background: #333;
+    color: #fff;
+    text-align: center;
+}
+`,e="",c=JSON.parse('{"title":"Табличная верстка","description":"","frontmatter":{"outline":[2,3]},"headers":[],"relativePath":"css/layout/page_004.md","filePath":"css/layout/page_004.md"}'),E={name:"css/layout/page_004.md"},b=Object.assign(E,{setup(r){return(d,i)=>(l(),n("div",null,[i[0]||(i[0]=p("",5)),t(a,{html:s(h),css:s(k),js:s(e),height:"500px"},null,8,["html","css","js"])]))}});export{c as __pageData,b as default};
