58306cd73bb0b830e2db23a94dd1b5f8315ec83e
MarkusFx <metroid-corp@yandex.ru>
2025-10-30 00:34:31 +0300

    Updates

---
+++ assets/css_property_page_013.md.CEzCU4Y_.lean.js
@@ -0,0 +1,22 @@
+import{C as a}from"./chunks/theme.CqPvG_5w.js";import{c as n,o as l,ag as p,G as t,k as s}from"./chunks/framework.ZCaT2mdU.js";const h=`<div class="scroll-box">
+    <div class="content">
+        Здесь содержимое, которое выходит за рамки контейнера, поэтому
+        появляется прокрутка по вертикали и горизонтали.
+    </div>
+</div>
+`,e=`.scroll-box {
+    width: 300px;
+    height: 150px;
+    border: 2px solid #333;
+    overflow: auto;
+    scroll-behavior: smooth;
+    white-space: nowrap; /* без переноса слов на новую строку */
+    padding: 10px;
+}
+
+.content {
+    width: 600px;
+    height: 300px;
+    background: linear-gradient(45deg, #00e5a4, transparent);
+}
+`,k="",c=JSON.parse('{"title":"Прокрутка","description":"","frontmatter":{"outline":[2,2]},"headers":[],"relativePath":"css/property/page_013.md","filePath":"css/property/page_013.md"}'),r={name:"css/property/page_013.md"},y=Object.assign(r,{setup(d){return(E,i)=>(l(),n("div",null,[i[0]||(i[0]=p("",6)),t(a,{html:s(h),css:s(e),js:s(k),height:"250px"},null,8,["html","css","js"])]))}});export{c as __pageData,y as default};
