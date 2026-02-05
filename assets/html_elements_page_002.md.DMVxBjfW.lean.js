f17420e53412e2433d40d3e8f88e74d0c14d8857
MarkusFx <metroid-corp@yandex.ru>
2026-02-05 03:41:10 +0300

    Updates

---
+++ assets/html_elements_page_002.md.DMVxBjfW.lean.js
@@ -0,0 +1,9 @@
+import{C as a}from"./chunks/theme.CmsK8ohK.js";import{c as t,o as n,ag as l,G as h,k as s}from"./chunks/framework.IX_DpZgh.js";const e=`<div>
+    <h1>Заголовок 1-го уровня</h1>
+    <h2>Заголовок 2-го уровня</h2>
+    <h3>Заголовок 3-го уровня</h3>
+    <h4>Заголовок 4-го уровня</h4>
+    <h5>Заголовок 5-го уровня</h5>
+    <h6>Заголовок 6-го уровня</h6>
+</div>
+`,p="",k="",o=JSON.parse('{"title":"Заголовки","description":"","frontmatter":{"outline":[2,3]},"headers":[],"relativePath":"html/elements/page_002.md","filePath":"html/elements/page_002.md"}'),E={name:"html/elements/page_002.md"},m=Object.assign(E,{setup(r){return(d,i)=>(n(),t("div",null,[i[0]||(i[0]=l("",4)),h(a,{html:s(e),css:s(p),js:s(k),height:"350px"},null,8,["html","css","js"])]))}});export{o as __pageData,m as default};
