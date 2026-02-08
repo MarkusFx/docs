da912063af868a4e3aeae54fdde5e1791e43bc04
MarkusFx <metroid-corp@yandex.ru>
2026-02-08 14:41:32 +0300

    Updates

---
+++ assets/css_property_page_014.md.M2qs5MzM.lean.js
@@ -0,0 +1,20 @@
+import{C as a}from"./chunks/theme.CimrRkTk.js";import{c as e,o as t,ag as n,G as l,k as s}from"./chunks/framework.IX_DpZgh.js";const p=`<div class="grad">Линейный градиент под углом -30°</div>
+`,h=`.grad {
+    width: 300px;
+    height: 150px;
+    background-image: linear-gradient(-30deg, #9e63dd, #9c27b0 10%, #00e5a4);
+    border: 1px solid #333;
+}
+`,r="",k=`<div class="rad-grad">Радиальный градиент</div>
+`,d=`.rad-grad {
+    width: 300px;
+    height: 150px;
+    background-image: radial-gradient(
+        circle at center,
+        #00e5a4,
+        #3e63dd,
+        #9c27b0
+    );
+    border: 1px solid #333;
+}
+`,g="",u=JSON.parse('{"title":"Градиент","description":"","frontmatter":{"outline":[2,2]},"headers":[],"relativePath":"css/property/page_014.md","filePath":"css/property/page_014.md"}'),o={name:"css/property/page_014.md"},F=Object.assign(o,{setup(c){return(E,i)=>(t(),e("div",null,[i[0]||(i[0]=n("",11)),l(a,{html:s(p),css:s(h),js:s(r),height:"250px"},null,8,["html","css","js"]),i[1]||(i[1]=n("",8)),l(a,{html:s(k),css:s(d),js:s(g),height:"250px"},null,8,["html","css","js"])]))}});export{u as __pageData,F as default};
